f1c47924c6f200a1c173be547146b760f92281da
Puneet Sharma <puneetpremsharma@hotmail.com>
2025-09-09 00:41:03 +0530

    added the java spring boot code

---
+++ src/main/java/com/spring_boot_starter_kit/spring_boot_starter_kit/SpringBootStarterKitApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_starter_kit.spring_boot_starter_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterKitApplication.class, args);
+	}
+
+}
